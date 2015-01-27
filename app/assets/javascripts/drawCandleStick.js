9f13b0bd10dd1855263912203cc54105f2dc755c
wingleungchoi <wingleungchoi@gmail.com>
2015-01-27 10:26:40 +0800

    able_to_toggle_tables

---
+++ app/assets/javascripts/drawCandleStick.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  var candleSticksJSBaby = JSON.parse(gon.candle_sticks);
+  var candleSticksJS = candleSticksJSBaby.map(function(dataCandleStick){
+    return {x: new Date(dataCandleStick.x), y: dataCandleStick.y};
+  });
+  var chart = new CanvasJS.Chart("chartContainer",
+  {
+    title:{
+      text: "CanvasJS Candlestick Chart",
+    },
+    exportEnabled: true,
+    axisY: {
+      includeZero: false,
+      prefix: "$",
+    },
+    axisX: {
+      valueFormatString: "DD-MMM",
+    },
+    data: [
+    {
+      type: "candlestick", 
+      dataPoints: candleSticksJS   
+
+    }
+    ]
+  });
+  chart.render();
+  };
---
+++ app/assets/javascripts/showTables.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#movingAvgButton").click(function(){
+    $("#movingAvgContainer").toggle();
+  });
+  $("#candleStickButton").click(function(){
+    $("#candleStickContainer").toggle();
+  });
+});
